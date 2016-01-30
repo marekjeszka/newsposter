bf72acfe9c6337b51dc566ad2d0764c2dab54392
Marek Jeszka <marek.jeszka@gmail.com>
2016-01-30 11:35:17 +0100

    Invoking POST in Wordpress implemented

--- src/main/java/com/jeszka/posters/WordPressQuickstart.java
+++ src/main/java/com/jeszka/posters/WordPressQuickstart.java
@@ -12,38 +12,56 @@ import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
 
 public class WordPressQuickstart {
-    private static final String BODY =
+    private static final String NEW_WORDPRESS_POST_FORMAT =
             "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n" +
-                    "<methodCall>\n" +
-                    "  <methodName>wp.getPost</methodName>\n" +
-                    "  <params>\n" +
-                    "   <param><value>1</value></param>\n" +
-                    "   <param><value>jeszkam</value></param>\n" +
-                    "   <param><value>" + System.getenv("NEWSPOSTER_WORDPRESS_PASSWORD") + "</value></param>\n" +
-                    "   <param><value>20</value></param>\n" +
-                    "  </params>\n" +
-                    "</methodCall>";
+                "<methodCall>\n" +
+                "  <methodName>wp.newPost</methodName>\n" +
+                "  <params>\n" +
+                "   <param><value>1</value></param>\n" +
+                "   <param><value>" + System.getenv("NEWSPOSTER_WORDPRESS_USERNAME") + "</value></param>\n" +
+                "   <param><value>" + System.getenv("NEWSPOSTER_WORDPRESS_PASSWORD") + "</value></param>\n" +
+                "   <struct>\n" +
+                "      <member>\n" +
+                "        <name>post_title</name>\n" +
+                "        <value>%s</value>\n" +
+                "      </member>\n" +
+                "      <member>\n" +
+                "        <name>post_content</name>\n" +
+                "        <value>%s</value>\n" +
+                "      </member>\n" +
+                "   </struct>\n" +
+                "  </params>\n" +
+                "</methodCall>";
 
-    public static void main(String[] args) {
+    private final WebTarget target;
+
+    public WordPressQuickstart() {
         ClientConfig config = new ClientConfig();
-
         Client client = ClientBuilder.newClient(config);
 
-        WebTarget target = client.target(getBaseURI());
-
-        String response = target
-                .request()
-                .post(Entity.entity(BODY, MediaType.TEXT_XML))
-                .toString();
-
-        System.out.println(response);
+        target = client.target(getBaseURI());
     }
 
     private static URI getBaseURI() {
         return UriBuilder.fromUri("http://localhost:8080/wordpress/xmlrpc.php").build();
     }
 
+    /**
+     * Invokes POST method to create a new post.
+     * @param post post to be created
+     */
     public void create(Post post) {
+        String postAsString = newWordpressPost(post.getTopic(), post.getBody());
 
+        String response = target
+                .request()
+                .post(Entity.entity(postAsString, MediaType.TEXT_XML))
+                .toString();
+
+        System.out.println(response);
+    }
+
+    private String newWordpressPost(String topic, String body) {
+        return String.format(NEW_WORDPRESS_POST_FORMAT, topic, body);
     }
 }
