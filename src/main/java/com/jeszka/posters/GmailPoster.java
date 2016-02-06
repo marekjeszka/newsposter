205083291a82dae9b1518cf670a0ce2e70eaa05f
Marek Jeszka <marek.jeszka@gmail.com>
2016-02-06 19:00:31 +0100

    Introduction to Gmail posting

---
+++ src/main/java/com/jeszka/posters/GmailPoster.java
@@ -0,0 +1,21 @@
+package com.jeszka.posters;
+
+import com.jeszka.domain.Post;
+
+public class GmailPoster implements Poster {
+    @Override
+    public boolean isAuthorized() {
+        // TODO check if exists ~/.credentials/newsposter/[name]
+        return false;
+    }
+
+    public boolean authorize() {
+        // TODO use client_secret.json and create file in .credentials (after authorization in Gmail)
+        return false;
+    }
+
+    @Override
+    public void create(Post post) {
+        // TODO invoke GmailQuickstart stuff
+    }
+}
