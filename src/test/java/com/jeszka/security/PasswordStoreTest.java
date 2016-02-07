805271c9d51c2dee8402494dd29bf6bbd29840df
Marek Jeszka <marek.jeszka@gmail.com>
2016-02-07 11:02:47 +0100

    Link to SHA1 generation added

--- src/test/java/com/jeszka/security/PasswordStoreTest.java
+++ src/test/java/com/jeszka/security/PasswordStoreTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertEquals;
 public class PasswordStoreTest {
     @Test
     public void encryptDecryptTest() throws GeneralSecurityException, IOException {
+        // https://commons.apache.org/proper/commons-codec/apidocs/src-html/org/apache/commons/codec/digest/DigestUtils.html#line.437
         String masterPassword = "512d60630ec3df27158620b1359173b884b0a551";
         String newPassword = "ddd123ddd";
         final String encrypted = PasswordStore.encrypt(masterPassword, newPassword);
